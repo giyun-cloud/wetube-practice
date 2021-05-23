353b6416c0cb627e2f4b0ce4bb719853c0ef2da0
giyun-cloud <nangiyoon@gmail.com>
2021-05-23 20:24:58 +0900

    TEMPLATES.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,12 @@
-export const home = (req, res) => res.render('home')
-export const see = (req, res) => res.render('watch')
-export const edit = (req, res) => res.send('<h1>Edit Video</h1>')
+const fakeUser = {
+  username: 'CloudG',
+  logined: true,
+}
+
+export const home = (req, res) =>
+  res.render('home', { pageTitle: 'Home', fakeUser })
+export const see = (req, res) => res.render('watch', { pageTitle: 'Watch' })
+export const edit = (req, res) => res.render('edit', { pageTitle: 'Edit' })
 export const search = (req, res) => res.send('<h1>Search Video</h1>')
 export const remove = (req, res) => res.send('<h1>Remove Video</h1>')
 export const upload = (req, res) => res.send('<h1>Upload Video</h1>')
