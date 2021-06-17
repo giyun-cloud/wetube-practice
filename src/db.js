e2269c09739d55bfa5a3a152e1f66c8194717ce2
giyun-cloud <nangiyoon@gmail.com>
2021-06-17 18:03:32 +0900

    17.6 Github and AWS S3 part One

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-})
+});
 
-const db = mongoose.connection
-const handledError = (err) => console.log('🔴 Error! : ', err)
-const handledOpen = () => console.log('🟢 Connected db')
+const db = mongoose.connection;
+const handledError = (err) => console.log("🔴 Error! : ", err);
+const handledOpen = () => console.log("🟢 Connected db");
 
-db.on('error', handledError)
-db.once('open', handledOpen)
+db.on("error", handledError);
+db.once("open", handledOpen);
